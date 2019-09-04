b9d528552c4addc20cb190784d7bd026636941a6
Scott Shawcroft <scott@tannewt.org>
2019-09-04 14:51:11 -0700

    Fix supporting FeatherWing and tweak breakout example

--- adafruit_ssd1675.py
+++ adafruit_ssd1675.py
@@ -49,7 +49,7 @@ __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_SSD1675.git"
 
 _START_SEQUENCE = (
-    b"\x12\x00" # Software reset
+    b"\x12\x80\x02" # Software reset, 2ms delay
     b"\x74\x01\x54" # set analog block control
     b"\x7e\x01\x3b" # set digital block control
     b"\x01\x03\xfa\x01\x00" # driver output control
@@ -74,10 +74,14 @@ _STOP_SEQUENCE = (
 class SSD1675(displayio.EPaperDisplay):
     """SSD1675 driver"""
     def __init__(self, bus, **kwargs):
-        color_command = None
-        super().__init__(bus, _START_SEQUENCE, _STOP_SEQUENCE, **kwargs,
+        stop_sequence = _STOP_SEQUENCE
+        try:
+            bus.reset()
+        except RuntimeError:
+            stop_sequence = b""
+        super().__init__(bus, _START_SEQUENCE, stop_sequence, **kwargs,
                          ram_width=160, ram_height=296,
                          set_column_window_command=0x44, set_row_window_command=0x45,
                          set_current_column_command=0x4e, set_current_row_command=0x4f,
-                         write_black_ram_command=0x24, write_color_ram_command=color_command,
-                         refresh_display_command=0x20)
+                         write_black_ram_command=0x24,
+                         refresh_display_command=0x20, refresh_time=2.2)
